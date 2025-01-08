5d795dade2ebe43af7d84fffff3c5a05123aeae4
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-08 14:55:08 +0530

    split

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,168 @@
-// const initialFriends = [
-//   {
-//     id: 118836,
-//     name: "Clark",
-//     image: "https://i.pravatar.cc/48?u=118836",
-//     balance: -7,
-//   },
-//   {
-//     id: 933372,
-//     name: "Sarah",
-//     image: "https://i.pravatar.cc/48?u=933372",
-//     balance: 20,
-//   },
-//   {
-//     id: 499476,
-//     name: "Anthony",
-//     image: "https://i.pravatar.cc/48?u=499476",
-//     balance: 0,
-//   },
-// ];
-
 import { useState } from "react";
-import Modal from "./Components/Modal.jsx";
 
-function App() {
-  const [opneModal, setOpenModal] = useState(false);
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({ children, onClick }) {
   return (
-    <div className="App">
-      <h1>Hey, Click on the button to open the modal.</h1>
-      <button className="openModalBtn" onClick={() => setOpenModal(true)}>Open</button>
-      {opneModal && <Modal closeModal={setOpenModal} />}
-    </div> 
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
-// function App() {
-//   return (
-//     <div className="app">
-//       <div className="sidebar">
-//         <FriendList />
-//       </div>
-//     </div>
-//   )
-// }
+function App() {
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
-// function FriendList() {
-//   const friends = initialFriends;
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
 
-//   return (
-//   <ul>{friends.map((friend) => (
-//     <li key={friend.id}>{friend.name}</li>
-//   )) }</ul>
-//   )
-// }
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false)
+  }
 
-// function Friend ({friend}) {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
 
-// }
+function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <div className="frinedlist">
+      <div>
+        <img src={friend.image} alt={friend.name} />
+      </div>
+      <div>
+        <h3>{friend.name}</h3>
+
+        {friend.balance < 0 && (
+          <p className="red">
+            You owe {friend.name} {friend.balance}$
+          </p>
+        )}
+        {friend.balance > 0 && (
+          <p className="green">
+            {" "}
+            {friend.name} owes you {friend.balance}$
+          </p>
+        )}
+        {friend.balance === 0 && (
+          <p className="red">
+            You and {friend.name} are even {friend.balance}$
+          </p>
+        )}
+      </div>
+      <Button>Select</Button>
+    </div>
+  );
+}
+
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}=${id}`,
+      balance: 0, 
+    };
+
+    console.log(newFriend);
+    onAddFriend(newFriend);
+     
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label> 🧑🏼‍🤝‍👩Friend Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>🌄 image URl</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a Bill With X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🕴🏾 Your Expense</label>
+      <input type="text" />
+
+      <label>👨🏻‍🤝‍👨🏼 X's Expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the Bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
 
 export default App;
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -27,3 +27,107 @@ function Modal({ closeModal }) {
 }
 
 export default Modal;
+
+
+/*
+
+App.js file
+
+import { useState } from "react";
+import Modal from "./Components/Modal.jsx";
+
+function App() {
+  const [opneModal, setOpenModal] = useState(false);
+  return (
+    <div className="App">
+      <h1>Hey, Click on the button to open the modal.</h1>
+      <button className="openModalBtn" onClick={() => setOpenModal(true)}>Open</button>
+      {opneModal && <Modal closeModal={setOpenModal} />}
+    </div> 
+  );
+}
+
+Modal.css file 
+
+.modalBackground {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .modalContainer {
+    width: 500px;
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    text-align: center;
+  }
+  
+  .title h1 {
+    margin-bottom: 10px;
+  }
+  
+  .body p {
+    margin-bottom: 20px;
+    font-size: 16px;
+    line-height: 1.5;
+    color: #333;
+  }
+  
+  .footer {
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .footer button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .footer button:hover {
+    opacity: 0.8;
+  }
+  
+  .footer button:first-child {
+    background-color: #f44336;
+    color: white;
+  }
+  
+  .footer button:last-child {
+    background-color: #4caf50;
+    color: white;
+  }
+  
+
+App.css
+
+.App {
+  display: flex;
+  flex-direction: column;
+  padding: 4rem;
+  height: 100vh;
+  align-items: center;
+}
+
+.App .openModalBtn {
+  background-color: cornflowerblue;
+  height: 2rem;
+  width: 100px;
+  border-radius: 8px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  font-size: medium;
+  
+}
+
+*/
